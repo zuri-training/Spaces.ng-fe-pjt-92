6e18ac7555fd32c5af72419f73194445ededb6da
Christian Madufor <chrismadufor@gmail.com>
2021-06-19 10:22:36 +0100

    Added Dashboard page
    The dashboard page is the catalogue for spaces and displays results based on the location input from the user. It also contains filters that can be used to make searching for spaces easier.

---
+++ assets/js/dashboard.js
@@ -0,0 +1,48 @@
+// MOBILE NAV MENU
+const menuIcon = document.getElementById('menu-icon');
+const mobileNav = document.getElementById('mobile-nav');
+const linksContainer = document.querySelector('#mobile-nav-links')
+const links = document.querySelectorAll('#mobile-nav-links li');
+const bar = document.getElementById('bars')
+
+menuIcon.addEventListener('click', () => { 
+    bar.classList.toggle('rotate')
+    mobileNav.classList.toggle('open')
+    if(mobileNav.classList.contains('bg-overlay')){
+    mobileNav.classList.remove('bg-overlay')
+    }else{ 
+    mobileNav.classList.add('bg-overlay')
+    }
+    linksContainer.classList.toggle('nav-animation')
+})
+function lockScroll() {
+    document.body.classList.toggle('lock-scroll');
+}
+
+// END 
+
+//  ASIDE ITEMS FOR SMALLER SCREENS
+const asideItem = document.querySelectorAll('.aside-item-title')
+const filterWrap = document.querySelectorAll('.filter-wrap')
+
+for (i = 0; i < asideItem.length; i++){
+    if(document.documentElement.clientWidth < 1150) {
+        let openFilter = asideItem[i];
+        openFilter.addEventListener('click', () => {
+            if(openFilter.nextElementSibling.classList.contains('close-filter')){
+                openFilter.nextElementSibling.classList.add ('open-filter')
+                openFilter.nextElementSibling.classList.remove ('close-filter')
+            }else if(openFilter.nextElementSibling.classList.contains('open-filter')) {
+                openFilter.nextElementSibling.classList.remove ('open-filter')
+                openFilter.nextElementSibling.classList.add ('close-filter')
+            }
+        })
+    }
+}
+
+// SPACE-ITEM LIKE ICON
+
+
+
+
+
