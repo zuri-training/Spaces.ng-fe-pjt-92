334489217c8db480897bae238a2c2d9078fa9781
Christian Madufor <chrismadufor@gmail.com>
2021-06-25 14:33:29 +0100

    Dashboard updates

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -19,6 +19,79 @@ function lockScroll() {
     document.body.classList.toggle('lock-scroll');
 }
 
+
+// POPULATING THE FORM WITH STATES AND LGAS
+
+function loadState() {
+    let dropdown = document.getElementById('state')
+    dropdown.length = 0
+
+    let defaultOption = document.createElement('option')
+    defaultOption.text = 'Select State'
+
+    dropdown.add(defaultOption)
+    dropdown.selectedIndex = 0
+
+    fetch('http://locationsng-api.herokuapp.com/api/v1/states')
+    .then(response => response.json())
+    .then(data => {
+        let option;
+
+        for (let i = 0; i < data.length; i++) {
+            option = document.createElement('option')
+            option.text = data[i].name
+            if (option.text !== 'Federal Capital Territory') {
+                option.value = data[i].name
+            } else {
+                option.value = 'Abuja'
+            }
+            dropdown.add(option)
+        }
+    })
+    .catch(error => {
+        console.log('Fetch Error: ', error)
+    })
+}
+
+
+function loadLga(state) {
+    let dropdown = document.getElementById('lga')
+    dropdown.length = 0
+
+    let defaultOption = document.createElement('option')
+    defaultOption.text = 'Select LGA'
+
+    dropdown.add(defaultOption)
+    dropdown.selectedIndex = 0
+
+
+    fetch(`http://locationsng-api.herokuapp.com/api/v1/states/${state}/lgas`)
+    .then(response => response.json())
+    .then(data => {
+       
+        let option;
+
+        for (let i = 0; i < data.length; i++) {
+            option = document.createElement('option')
+            option.text = data[i]
+            option.value = data[i]
+            dropdown.add(option)
+        }
+    })
+    .catch(error => {
+        console.log('Fetch Error: ', error)
+    })
+}
+
+document.addEventListener('DOMContentLoaded', loadState)
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('#state').onchange = function () {
+        let state = this.value
+        loadLga(state)
+    }
+})
+
+
 // END 
 
 //  ASIDE ITEMS FOR SMALLER SCREENS
