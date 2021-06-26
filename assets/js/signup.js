03aecb51b48e42b5e5b62cd73796462b4e919eed
Christian Madufor <chrismadufor@gmail.com>
2021-06-26 07:40:58 +0100

    Sign up page updates

--- assets/js/signup.js
+++ assets/js/signup.js
@@ -1,10 +1,26 @@
-const navToggler = document.querySelector('.nav-toggler');
-const navLinks = document.querySelector('.nav-links-container');
+// HEADER STYLES
+// MOBILE NAV MENU
+const menuIcon = document.getElementById('menu-icon');
+const mobileNav = document.getElementById('mobile-nav');
+const linksContainer = document.querySelector('#mobile-nav-links')
+const links = document.querySelectorAll('#mobile-nav-links li');
+const bar = document.getElementById('bars')
 
-navToggler.addEventListener('click', () => {
- navLinks.classList.toggle('active');
- 
-});
+menuIcon.addEventListener('click', () => { 
+    bar.classList.toggle('rotate')
+    mobileNav.classList.toggle('open')
+    if(mobileNav.classList.contains('bg-overlay')){
+    mobileNav.classList.remove('bg-overlay')
+    }else{ 
+    mobileNav.classList.add('bg-overlay')
+    }
+    linksContainer.classList.toggle('nav-animation')
+})
+function lockScroll() {
+    document.body.classList.toggle('lock-scroll');
+}
+
+
 
 var checkP = function(){
     if (document.getElementById('password').value=== document.getElementById('confirm').value){
