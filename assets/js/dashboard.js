fc1568df785216fd371aa8be44640ee3f2e5717b
Christian Madufor <chrismadufor@gmail.com>
2021-07-06 10:28:52 +0100

    updates to the dashboard page

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -6,6 +6,7 @@ const links = document.querySelectorAll('#mobile-nav-links li');
 const bar = document.getElementById('bars')
 
 menuIcon.addEventListener('click', () => { 
+    document.body.classList.toggle('lock-scroll')
     bar.classList.toggle('rotate')
     mobileNav.classList.toggle('open')
     if(mobileNav.classList.contains('bg-overlay')){
@@ -15,9 +16,82 @@ menuIcon.addEventListener('click', () => {
     }
     linksContainer.classList.toggle('nav-animation')
 })
-function lockScroll() {
-    document.body.classList.toggle('lock-scroll');
+// function lockScroll() {
+//     document.body.classList.toggle('lock-scroll');
+// }
+
+
+// POPULATING THE FORM WITH STATES AND LGAS
+
+function loadState() {
+    let dropdown = document.getElementById('state')
+    dropdown.length = 0
+
+    let defaultOption = document.createElement('option')
+    defaultOption.text = 'Select State'
+
+    dropdown.add(defaultOption)
+    dropdown.selectedIndex = 0
+
+    fetch('NgStateAndLgaApi.txt')
+    .then(response => response.json())
+    .then(data => {
+        let option;
+
+        for (let i = 0; i < data.length; i++) {
+            option = document.createElement('option')
+            option.text = data[i].state.name
+            option.value = data[i].state.id
+            dropdown.add(option)
+        }
+    })
+    .catch(error => {
+        console.log('Fetch Error: ', error)
+    })
 }
+
+
+function loadLga(state) {
+    let dropdown = document.getElementById('lga')
+    dropdown.length = 0
+
+    let defaultOption = document.createElement('option')
+    defaultOption.text = 'Select LGA'
+
+    dropdown.add(defaultOption)
+    dropdown.selectedIndex = 0
+
+
+    fetch('NgStateAndLgaApi.txt')
+        .then(response => response.json())
+        .then(data => {
+        
+        let option;
+
+        for (let i = 0; i < data.length; i++) {
+            if (parseInt(state) === data[i].state.id) {
+                for (let j = 0; j < data[i].state.locals.length; j++) {
+                    option = document.createElement('option')
+                    option.text = data[i].state.locals[j].name
+                    option.value = data[i].state.locals[j].id
+                    dropdown.add(option)
+                }
+            }
+        }
+    })
+    .catch(error => {
+        console.log('Fetch Error: ', error)
+    })
+}
+
+document.addEventListener('DOMContentLoaded', loadState)
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('#state').onchange = function () {
+        let state = this.value
+        loadLga(state)
+    }
+})
+
 
 // END 
 
@@ -47,7 +121,8 @@ const spacesItem = document.querySelectorAll('.spaces-item')
 function likeBtn (){
     [...spacesItem].forEach((item) => {
         let like = item.children[2]
-        like.addEventListener('click', () => {
+        like.addEventListener('click', (e) => {
+            e.preventDefault()
             if (like.innerHTML === '<i class="far fa-heart"></i>'){
                 like.innerHTML = '<i class="fas fa-heart liked"></i>';
             }
@@ -61,4 +136,18 @@ function likeBtn (){
 likeBtn();
 
 
+// FUNCTION FOR NO RESULTS
 
+const noResultWrap = document.querySelector('.no-results')
+const main = document.querySelector('.main')
+
+function noSearch () {
+    if (noResultWrap.style.display !== 'none') {
+        if(document.documentElement.clientWidth < 7600){
+            main.style.paddingBottom = '50px'
+        }else{
+            main.style.paddingBottom = '150px'
+        } 
+    }
+}
+noSearch();
